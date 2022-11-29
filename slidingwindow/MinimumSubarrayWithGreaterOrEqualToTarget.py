36ca11edeeedb0c2e05c0ddfa481eb976956ce3d
Aakash Verma <info.aakash11@gmail.com>
2022-11-29 18:14:21 +0530

    Update and rename MinimumSubarrayWithTarget.py to MinimumSubarrayWithGreaterOrEqualToTarget.py

--- slidingwindow/MinimumSubarrayWithGreaterOrEqualToTarget.py
+++ slidingwindow/MinimumSubarrayWithGreaterOrEqualToTarget.py
@@ -1,13 +1,17 @@
 import math
-class MinSubArrayLenForTarget:
-    def minSubArrayLen(nums, target):
+
+class MinimumSubarrayWithGreaterOrEqualToTarget:
+
+    def min_subarray(nums, target):
+        
         window_start = 0
         window_sum = 0
-        minimum_length = math.inf 
+        minimum_length = math.inf
+        
         for window_end in range(len(nums)):
             window_sum += nums[window_end]
             while window_sum >= target:
-                minimum_length = min(minimum_length,(window_end-window_start+1))
+                minimum_length = min(minimum_length, window_end - window_start + 1)
                 window_sum -= nums[window_start]
                 window_start += 1
         
@@ -15,8 +19,10 @@ class MinSubArrayLenForTarget:
             return minimum_length
         else:
             return 0 
+            
 if __name__== '__main__':
-    nums = [2,3,1,2,4,3]
+    
+    nums = [2, 3, 1, 2, 4, 3]
     target = 7
-    minimum_length = MinSubArrayLenForTarget.minSubArrayLen(nums,target)
+    minimum_length = MinimumSubarrayWithGreaterOrEqualToTarget.min_subarray(nums,target)
     print(minimum_length)
