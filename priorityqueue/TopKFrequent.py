c56453cb160015f12474606e4b1092506a8aeeff
Deepanshu Sharma <92631184+deepanshu-sharmaa@users.noreply.github.com>
2022-12-18 19:16:03 +0530

    Add files via upload

---
+++ priorityqueue/TopKFrequent.py
@@ -0,0 +1,32 @@
+import heapq
+
+class TopFrequentElement:
+
+    @staticmethod
+    def topKFrequent(nums, k):
+
+        hash_map = dict()
+        for i in nums:
+            if i not in hash_map:
+                hash_map[i] = 1
+            else:
+                hash_map[i] += 1
+
+        max_heap = []
+        heapq.heapify(max_heap)
+
+        for key, value in hash_map.items():
+            heapq.heappush(max_heap, (-value, key))
+
+        res = list()
+        while len(max_heap)>0:
+            value, key = heapq.heappop(max_heap)
+            res.append(key)
+
+        return res[:k]
+
+if __name__ == '__main__':
+	nums = [1,1,1,2,2,3]
+	k = 2
+	ans = TopFrequentElement.topKFrequent(nums, k)
+	print(ans)
